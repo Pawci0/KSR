5a4129c9971c604ebd6024dc696b854eea9adb3a
P G <pgalewicz@gmail.com>
2019-05-05 21:25:41 +0200

    added more methods in ExtractorFactory

--- src/main/java/com/ksr/feature_extraction/ExtractorFactory.java
+++ src/main/java/com/ksr/feature_extraction/ExtractorFactory.java
@@ -2,13 +2,13 @@ package com.ksr.feature_extraction;
 
 import com.ksr.data_preparation.Article;
 import com.ksr.feature_extraction.basic.*;
-import com.ksr.feature_extraction.keyword.ClassKeywordOccurrenceExtractor;
+import com.ksr.feature_extraction.keyword.*;
 
 import java.util.List;
 
 public class ExtractorFactory {
 
-    public static Extractor AllExtractors(List<Article> trainingSet, int keywordCount){
+    public static Extractor AllGeneralExtractors(List<Article> trainingSet, int keywordCount){
         return new MixedExtractor(new ClassKeywordOccurrenceExtractor(trainingSet, keywordCount),
                 new ArticleLengthExtractor(),
                 new AvgWordLengthExtractor(),
@@ -18,12 +18,36 @@ public class ExtractorFactory {
                 new LabelOccurenceExtractor());
     }
 
+    public static Extractor AllKeywordExtractors(List<Article> trainingSet, int keywordCount){
+        return new MixedExtractor(new ClassKeywordOccurrenceExtractor(trainingSet, keywordCount),
+                new AnyKeywordAppeared(),
+                new AverageKeywordAppearance(),
+                new HowManyKeywordsAppeared(),
+                new HowManyTimesKeywordsAppeared(),
+                new MostCommonKeywordOccurences(),
+                new StartingKeywordsExtractor(),
+                new WeightedKeywordAppearance()
+                );
+    }
+
+    public static Extractor KeywordExtractors(){
+        return new MixedExtractor(
+                new AnyKeywordAppeared(),
+                new AverageKeywordAppearance(),
+                new HowManyKeywordsAppeared(),
+                new HowManyTimesKeywordsAppeared(),
+                new MostCommonKeywordOccurences(),
+                new StartingKeywordsExtractor(),
+                new WeightedKeywordAppearance()
+        );
+    }
+
     public static Extractor GeneralExtractors(){
         return new MixedExtractor(new ArticleLengthExtractor(), new AvgWordLengthExtractor(), new LabelOccurenceExtractor(),
                 new MostCommonBigLetterExtractor(), new UniqueWordCountExtractor(), new UpperCaseExtractor());
     }
 
-    public static Extractor KeywordExtractor(List<Article> trainingSet, int keywordCount){
+    public static Extractor ClassKeywordExtractor(List<Article> trainingSet, int keywordCount){
         return new ClassKeywordOccurrenceExtractor(trainingSet, keywordCount);
     }
 
