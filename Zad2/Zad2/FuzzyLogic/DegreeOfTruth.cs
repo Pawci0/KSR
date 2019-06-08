cff7c26c5013d53f12abd5a7a028c3c08c2e2a66
Szymon <szymonicho@gmail.com>
2019-06-08 18:20:02 +0200

    database, fuzzy logic initial work

---
+++ Zad2/Zad2/FuzzyLogic/DegreeOfTruth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad2.FuzzyLogic
+{
+    public class DegreeOfTruth
+    {
+        public double CalculateDOT(LinguisticVariable quantificator, LinguisticVariable qualifier, LinguisticVariable summarizer)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Zad2/Zad2/FuzzyLogic/LinguisticVariable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zad2.Membership;
+
+namespace Zad2.FuzzyLogic
+{
+    public class LinguisticVariable
+    {
+        public string Name { get; set; }
+        public IMembershipFunction MembershipFunction { get; set; }
+        public List<double> MembershipFunctionParameters { get; set; }
+        public string MemberToExtract { get; set; }
+
+    }
+}
