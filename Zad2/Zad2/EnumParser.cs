487ba1953d4a35b0a085c0f0d26487f82a3d89c3
Szymon <szymonicho@gmail.com>
2019-06-08 19:50:48 +0200

    aha

---
+++ Zad2/Zad2/EnumParser.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad2.DataModel.Enums
+{
+    static class EnumParser
+    {
+        public static void Parse(string value, out MeetName ret)
+        {
+            if (value.Equals("Collegiate Nationals"))
+            {
+                ret = MeetName.Collegiate_Nationals;
+            }
+            else if (value.Equals("European Championships"))
+            {
+                ret = MeetName.European_Championships;
+            }
+            else if (value.Equals("Nationals"))
+            {
+                ret = MeetName.Nationals;
+            }
+            else if (value.Equals("Raw Nationals"))
+            {
+                ret = MeetName.Raw_Nationals;
+            }
+            else if (value.Equals("Russian Powerlifting Championships"))
+            {
+                ret = MeetName.Russian_Powerlifting_Championships;
+            }
+            else if (value.Equals("World Championships"))
+            {
+                ret = MeetName.World_Championships;
+            }
+            else if (value.Equals("World Cup"))
+            {
+                ret = MeetName.World_Cup;
+            }
+            else if (value.Equals("World Masters Powerlifting Championships"))
+            {
+                ret = MeetName.World_Masters_Powerlifting_Championships;
+            }
+            else
+            {
+                ret = MeetName.World_Powerlifting_Championships;
+            }
+        }
+
+        public static void Parse(string value, out Equipment ret)
+        {
+            if (value.Equals("Multi-ply"))
+            {
+                ret = Equipment.Multi_ply;
+            }
+            else if (value.Equals("Raw"))
+            {
+                ret = Equipment.Raw;
+            }
+            else if (value.Equals("Single-ply"))
+            {
+                ret = Equipment.Single_ply;
+            }
+            else
+            {
+                ret = Equipment.Wraps;
+            }
+        }
+    }
+}
---
+++ Zad2/Zad2/FuzzyLogic/StaticVariables.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zad2.Membership;
+
+namespace Zad2.FuzzyLogic
+{
+    public static class StaticVariables
+    {
+        #region Age
+        public static LinguisticVariable ageYoung = new LinguisticVariable
+        {
+            Name = "Young",
+            MemberToExtract = "Age",
+            MembershipFunction = new TrapezoidFunction(),
+            MembershipFunctionParameters = new List<double> { 8, 18 }
+        };
+        public static LinguisticVariable ageYoungAdult = new LinguisticVariable
+        {
+            Name = "Young Adult",
+            MemberToExtract = "Age",
+            MembershipFunction = new TrapezoidFunction(),
+            MembershipFunctionParameters = new List<double> { 19, 30 }
+        };
+        public static LinguisticVariable ageAdult = new LinguisticVariable
+        {
+            Name = "Adult",
+            MemberToExtract = "Age",
+            MembershipFunction = new TrapezoidFunction(),
+            MembershipFunctionParameters = new List<double> { 31, 50 }
+        };
+        public static LinguisticVariable ageOld = new LinguisticVariable
+        {
+            Name = "Old",
+            MemberToExtract = "Age",
+            MembershipFunction = new TrapezoidFunction(),
+            MembershipFunctionParameters = new List<double> { 51, 90 }
+        };
+        #endregion
+    }
+}
