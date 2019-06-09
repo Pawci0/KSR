0c5d7b0ba18d95ee9a6dd84b7cb42b60d0701477
Szymon <szymonicho@gmail.com>
2019-06-09 12:27:41 +0200

    button

--- Zad2/Zad2/ViewModel/MainViewModel.cs
+++ Zad2/Zad2/ViewModel/MainViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using Zad2.DataModel;
 using Zad2.FuzzyLogic;
 
@@ -16,6 +18,8 @@ namespace Zad2.ViewModel
 
         public LinguisticVariable SelectedQualifier { get; set; }
         public LinguisticVariable SelectedSummarizer { get; set; }
+        private ObservableCollection<LinguisticVariable> quantifiers;
+        public ICommand GenerateCommand { get; set; }
 
         public MainViewModel()
         {
@@ -25,11 +29,21 @@ namespace Zad2.ViewModel
         {
             this.dataContext = dataContext;
             LinguisticVariables = StaticVariables.getAllVariables();
+            quantifiers = StaticQuantifiers.getAllQuantifiers();
+            GenerateCommand = new RelayCommand(o => Generate());
         }
 
-        public void generate()
+        private void Generate()
         {
-
+            List<KeyValuePair<double, string>> summaries = new List<KeyValuePair<double, string>>();
+            foreach (var quantifier in quantifiers)
+            {
+                summaries.Add(new KeyValuePair<double, string>(
+                    DegreeOfTruth.CalculateDOT(quantifier, SelectedQualifier, SelectedSummarizer, dataContext.Entry.ToList()),
+                    quantifier.Name + " people being/having " + SelectedQualifier.MemberAndName + " are/have " + SelectedSummarizer.MemberAndName));
+            }
+            summaries.Sort((x, y) => y.Key.CompareTo(x.Key));
+            int i = 1;
         }
     }
 }
---
+++ Zad2/Zad2/ViewModel/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Zad2.ViewModel
+{
+    public class RelayCommand : ICommand
+{
+    private readonly Action<object> _execute;
+    private readonly Predicate<object> _canExecute;
+
+    public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+    {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+        return _canExecute == null || _canExecute(parameter);
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+
+    public void Execute(object parameter)
+    {
+        _execute(parameter ?? "<N/A>");
+    }
+
+}
+}
