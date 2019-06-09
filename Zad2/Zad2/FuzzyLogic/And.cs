da7d2c1d19c3facee29d596a8c321a0bb71f9505
Szymon <szymonicho@gmail.com>
2019-06-09 18:57:41 +0200

    rework

---
+++ Zad2/Zad2/FuzzyLogic/And.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Zad2.DataModel;
+using Zad2.Membership;
+
+namespace Zad2.FuzzyLogic
+{
+    class And : FuzzySet
+    {
+        FuzzySet fuzzySet1;
+        FuzzySet fuzzySet2;
+
+        public new double GetMembership(Entry entry)
+        {
+            return Math.Min(fuzzySet1.GetMembership(entry), fuzzySet2.GetMembership(entry));
+        }
+
+        public new List<Entry> Support(List<Entry> entries)
+        {
+            return fuzzySet1.Support(entries).Intersect(fuzzySet2.Support(entries)).ToList(); ;
+        }
+
+        public new double DegreeOfFuzziness(List<Entry> entries)
+        {
+            return fuzzySet1.DegreeOfFuzziness(entries) * fuzzySet2.DegreeOfFuzziness(entries);
+        }
+    }
+}
---
+++ Zad2/Zad2/FuzzyLogic/Or.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Zad2.DataModel;
+using Zad2.Membership;
+
+namespace Zad2.FuzzyLogic
+{
+    class Or : FuzzySet
+    {
+        FuzzySet fuzzySet1;
+        FuzzySet fuzzySet2;
+
+        public new double GetMembership(Entry entry)
+        {
+            return Math.Max(fuzzySet1.GetMembership(entry), fuzzySet2.GetMembership(entry));
+        }
+
+        public new List<Entry> Support(List<Entry> entries)
+        {
+            return fuzzySet1.Support(entries).Concat(fuzzySet2.Support(entries)).Distinct().ToList();
+        }
+
+        public new double DegreeOfFuzziness(List<Entry> entries)
+        {
+            return fuzzySet1.DegreeOfFuzziness(entries) * fuzzySet2.DegreeOfFuzziness(entries);
+        }
+    }
+}
